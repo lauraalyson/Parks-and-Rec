488522ef528d6aabc61a88152bd00e4018d14e5b
lauraalyson <lauraalyson3@gmail.com>
2021-11-23 13:43:55 -0500

    add new app

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react';
+import {Text, View} from 'react-native';
 import colors from './assets/colors/colors';
-import {StyleSheet, Text, View} from 'react-native';
 import 'react-native-gesture-handler';
 
 const App = () => {
   return <Text>This is text</Text>;
 };
-
-const styles = StyleSheet.create({
-  tabBar: {
-    backgroundColor: colors.white,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-});
 
 export default App;
